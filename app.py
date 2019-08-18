54b583c9c313aeb8133d4031b508388a6094f734
James Sully <sullyj3@gmail.com>
2019-08-19 01:37:55 +1000

    Use gunicorn for deployment

--- app.py
+++ app.py
@@ -9,15 +9,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-def get_port() -> int:
-    try:
-        port = os.environ['PORT']
-        print(f'Environment says to run on port {port}')
-        return int(port)
-    except KeyError as e:
-        DEFAULT_PORT = 5000
-        print(f'PORT environment variable not found, using fallback {DEFAULT_PORT}')
-        return DEFAULT_PORT
-
 if __name__ == '__main__':
-    app.run(port=get_port())
+    app.run()
