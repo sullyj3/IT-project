7f208c38bea9c9296363871216ae2b71cbc348a2
James Sully <sullyj3@gmail.com>
2019-08-19 01:20:15 +1000

    Get the correct port to run on from heroku via env

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+import os
+
 app = Flask(__name__)
 
 
@@ -7,6 +9,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+def get_port() -> int:
+    try:
+        return os.environ['PORT']
+    except KeyError as e:
+        DEFAULT_PORT = 5000
+        print(f'PORT environment variable not found, using fallback {DEFAULT_PORT}')
+        return DEFAULT_PORT
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=get_port())
