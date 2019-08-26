9bdf83d0f36a7fb321ba95d9df04832b03d636df
James Sully <sullyj3@gmail.com>
2019-08-26 14:56:41 +1000

    Connnect to postgres and print out some example data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
-
 import os
+
+from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
 
@@ -9,11 +10,25 @@ app = Flask(__name__)
 def hello_world():
     return '🐢 Hello furious turtles! 🐢'
 
+def print_dummy_data(conn) -> None:
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM ITProjectTestTable;')
+    data = cur.fetchall()
+
+    print("\ndummy data:")
+    for id, text in data:
+        print(id, text)
+    print()
+
 if __name__ == '__main__':
     db_URL = os.environ.get("DATABASE_URL")
     if db_URL is None:
         print("DATABASE_URL not found!")
     else:
         print(f"DATABASE_URL is '{db_URL}'")
+
+    conn = psycopg2.connect(db_URL)
+    print(f"Successfully connected to the database!")
+    print_dummy_data(conn)
 
     app.run()
