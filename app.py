20c01569f17cac5fb8d58a2f02b709e787494612
James Sully <sullyj3@gmail.com>
2019-08-26 12:00:23 +1000

    Read DATABASE_URL from environment

--- app.py
+++ app.py
@@ -10,4 +10,10 @@ def hello_world():
     return '🐢 Hello furious turtles! 🐢'
 
 if __name__ == '__main__':
+    db_URL = os.environ.get("DATABASE_URL")
+    if db_URL is None:
+        print("DATABASE_URL not found!")
+    else:
+        print(f"DATABASE_URL is '{db_URL}'")
+
     app.run()
