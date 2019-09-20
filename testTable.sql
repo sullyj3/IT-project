af3036cc74a5a31d10c0a3951ef56e17d5b16c7e
James Sully <sullyj3@gmail.com>
2019-09-20 14:38:40 +1000

    Remove test table schema

--- testTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ITProjectTestTable (
-    id        serial PRIMARY KEY,
-    someText    text NOT NULL
-);
-
-INSERT INTO ITProjectTestTable (someText)
-VALUES ('example text 1'), ('lorem ipsum');
