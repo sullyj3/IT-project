3e64a12c398dd8388ae6750701dc536e10ce91f0
Dandy <gellienathan@hotmail.com.au>
2019-10-19 12:19:52 +1100

    prepformerge

--- views.py
+++ views.py
@@ -7,7 +7,4 @@ def view_artefacts(artefacts: List[Artefact]) -> str:
     return render_template('artefacts_template.html', artefacts=artefacts)
 
 def view_artefact(artefact: Artefact, artefact_images: [ArtefactImage]) -> str:
-    with open('views/artefact.html', encoding='utf8') as f:
-        template = Template(f.read())
-    return template.render(artefact=artefact,
-                           artefact_images=artefact_images)
+    return render_template("artefact.html", artefact=artefact, artefact_images=artefact_images)
