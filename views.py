af7930b115b014fd1ffb68e7f8580c8a84d5e344
thamilton1 <thomguy@applebox.com.au>
2019-10-19 15:25:49 +1100

    base html used on artefact_view

--- views.py
+++ views.py
@@ -7,4 +7,4 @@ def view_artefacts(artefact_previews: List[Dict]) -> str:
     return render_template('artefacts_template.html', artefact_previews=artefact_previews)
 
 def view_artefact(artefact: Artefact, artefact_images: [ArtefactImage]) -> str:
-    return render_template("artefact.html", artefact=artefact, artefact_images=artefact_images)
+    return render_template("artefact_view.html", artefact=artefact, artefact_images=artefact_images)
